8fe665afff636b338186c9558e687fc98c18fdbb
SLajuwomi <stephenlajuwomi@gmail.com>
2026-01-12 14:47:52 -0600

    initial scaffolding for SpringBoot project. generated by start.spring.io, learned about this from devtiro springboot tutorial

---
+++ src/test/java/com/slajuwomi/realtime_zetamac/RealtimeZetamacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slajuwomi.realtime_zetamac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeZetamacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
