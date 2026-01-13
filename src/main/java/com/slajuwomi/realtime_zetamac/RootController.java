0788cf363785869b40d705a8f0195ecf9def67bb
SLajuwomi <stephenlajuwomi@gmail.com>
2026-01-12 18:35:38 -0600

    trying to follow a blog post on SpringBoot+Vue.js, seems to be outdated, and I can't get it to work

---
+++ src/main/java/com/slajuwomi/realtime_zetamac/RootController.java
@@ -0,0 +1,14 @@
+package com.slajuwomi.realtime_zetamac;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("eventName", "FIFA 2018");
+        return "index";
+    }
+}
