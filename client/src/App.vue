2e98030b1bc895e519f88b5900453a0fbee7905e
SLajuwomi <stephenlajuwomi@gmail.com>
2026-01-15 16:44:30 -0600

    moved the stuff I did yesterday to the Vue app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,47 +1,135 @@
+<!--<script setup>-->
+<!--import HelloWorld from './components/HelloWorld.vue'-->
+<!--import TheWelcome from './components/TheWelcome.vue'-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <header>-->
+<!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
+
+<!--    <div class="wrapper">-->
+<!--      <HelloWorld msg="You did it!" />-->
+<!--    </div>-->
+<!--  </header>-->
+
+<!--  <main>-->
+<!--    <TheWelcome />-->
+<!--  </main>-->
+<!--</template>-->
+
+<!--<style scoped>-->
+<!--header {-->
+<!--  line-height: 1.5;-->
+<!--}-->
+
+<!--.logo {-->
+<!--  display: block;-->
+<!--  margin: 0 auto 2rem;-->
+<!--}-->
+
+<!--@media (min-width: 1024px) {-->
+<!--  header {-->
+<!--    display: flex;-->
+<!--    place-items: center;-->
+<!--    padding-right: calc(var(&#45;&#45;section-gap) / 2);-->
+<!--  }-->
+
+<!--  .logo {-->
+<!--    margin: 0 2rem 0 0;-->
+<!--  }-->
+
+<!--  header .wrapper {-->
+<!--    display: flex;-->
+<!--    place-items: flex-start;-->
+<!--    flex-wrap: wrap;-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref, watch} from 'vue'
+
+let firstNumber = ref(0)
+let secondNumber = ref(0)
+let signs = ["+", "-", "*", "/"]
+let sign = ref("")
+let correctAnswer = 0
+let userAnswer = ref()
+
+function generateNewProblem() {
+  firstNumber.value = Math.floor(Math.random() * 10)
+  secondNumber.value = Math.floor(Math.random() * 10)
+  sign.value = signs[Math.floor(Math.random() * 4)]
+  userAnswer.value = ""
+  generateCorrectAnswer()
+}
+
+function generateCorrectAnswer() {
+  if (sign.value === "+") {
+    correctAnswer = firstNumber.value + secondNumber.value
+  } else if (sign.value === "*") {
+    correctAnswer = firstNumber.value * secondNumber.value
+  } else if (sign.value === "/") {
+    correctAnswer = Math.floor(firstNumber.value / secondNumber.value)
+  } else if (sign.value === "-") {
+    correctAnswer = firstNumber.value - secondNumber.value
+  }
+}
+
+// console.log("correct answer", correctAnswer)
+watch(userAnswer, (v) => {
+  // console.log(Number(v))
+  if (Number(v) === correctAnswer) {
+    // console.log("here")
+    generateNewProblem()
+  }
+})
+
+generateNewProblem()
+
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="vue-3">
+    <div id="input-container">
+      <label for="answer-box">{{ firstNumber }} {{ sign }} {{ secondNumber }}</label>
+      <input v-model="userAnswer" type="text" id="answer-box" name="answer-box" value=""/>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+#app {
+  width: 100%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body {
+  height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  font-size: 48px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+#vue-3 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+#input-container {
+  display: flex;
+  background-color: #dddddd;
+  padding: 5px;
+  width: 100%;
+  justify-content: center;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#input-container label {
+  margin-right: 15px;
+}
+
+input {
+  width: 180px;
+  font-size: 48px;
 }
 </style>
